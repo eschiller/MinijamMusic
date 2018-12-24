2a9cdc354b99b647bfc0283641f370bb64130ddc
eschiller <eric.schiller@raytheon.com>
2018-12-24 17:42:11 -0500

    game manager in place

---
+++ Assets/Scripts/Prefabs/GameManagement/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Vector3 myPos;
+	public Transform myPlay;
+
+	// Use this for initialization
+	void Start () {
+	}
+	//var myPos : Vector3;
+	//var myPlay : Transform;
+
+
+
+	void Update()
+	{
+		transform.position = myPlay.position + myPos;
+
+	}
+}
