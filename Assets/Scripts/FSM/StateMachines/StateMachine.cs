4540e9a5a68fcb2a58e161d9a08b306c784c13f7
eschiller <eric.schiller@raytheon.com>
2018-12-26 15:57:14 -0500

    adding state machine primitives

---
+++ Assets/Scripts/FSM/StateMachines/StateMachine.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMachine : MonoBehaviour {
+	public IState startingState;
+	private IState currentState;
+	private IState previousState;
+
+    private void Start()
+    {
+        currentState = startingState;
+        changeState(startingState);
+    }
+
+    public virtual void changeState(IState newState) {
+		previousState = currentState;
+		currentState.exitState ();
+		currentState = newState;
+        currentState.setStateMachine(this);
+		currentState.enterState ();
+	}
+
+	public virtual void revertState() {
+		IState tempState = currentState;
+		currentState.exitState ();
+		currentState = previousState;
+		previousState = tempState;
+        currentState.setStateMachine(this);
+		currentState.enterState ();
+	}
+}
---
+++ Assets/Scripts/FSM/States/IState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface IState {
+	void updateState ();
+	void setTransitions(Dictionary<string, IState> transitionStates);
+	void setStateMachine(StateMachine theMachine);
+	void enterState();
+	void exitState();
+}
