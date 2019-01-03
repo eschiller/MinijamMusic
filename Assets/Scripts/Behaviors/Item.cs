8e5ab05b18a0f7013b2916a810585f71c1402b5f
eschiller <eric.schiller@raytheon.com>
2019-01-02 19:46:49 -0500

    adding behaviors and changing project to default to unix end-lines

---
+++ Assets/Scripts/Behaviors/Item.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public Transform ownerObject;
+
+    PlatformerPhysics pphys;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        pphys = GetComponent<PlatformerPhysics>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ownerObject)
+        {
+            transform.position = ownerObject.position;
+        }
+    }
+
+    public void grabItem(Transform newOwner) {
+        pphys.disable();
+        ownerObject = newOwner;
+    }
+
+    public void throwItem(Vector2 direction) {
+        Debug.Log("throwing item");
+        ownerObject = null;
+        pphys.enable();
+        pphys.setXVel(2.0f * direction.x);
+    }
+}
+
