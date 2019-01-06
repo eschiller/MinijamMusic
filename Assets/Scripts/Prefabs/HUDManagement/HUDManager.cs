4ad0243baaa4478ffa8c0b266e54aedc3625e4c7
eschiller <eric.schiller@raytheon.com>
2019-01-06 15:42:28 -0500

    adding healthicon prefab as well as sethealth function in HUD

--- Assets/Scripts/Prefabs/HUDManagement/HUDManager.cs
+++ Assets/Scripts/Prefabs/HUDManagement/HUDManager.cs
@@ -7,10 +7,13 @@ using UnityEngine.UI;
 public class HUDManager : MonoBehaviour {
 
     Text middleText;
+    public GameObject healthIcon;
+    GameObject allCanvas;
 
 	// Use this for initialization
 	void Start () {
-        middleText = transform.Find("TextCanvas/MiddleText").GetComponent<Text>();
+        allCanvas = GameObject.Find("AllCanvas");
+        middleText = transform.Find("AllCanvas/MiddleText").GetComponent<Text>();
         middleText.enabled = false;
     }
 
@@ -28,5 +31,14 @@ public class HUDManager : MonoBehaviour {
     {
         middleText.text = "";
         middleText.enabled = false;
+    }
+
+
+    public void SetHealth(int health, int xPadding = 16, int yPadding = -16, int spacing = 48) {
+        for (int i = 0; i < health; i++) {
+            GameObject aHeart;
+            aHeart = Instantiate(healthIcon, new Vector3((xPadding + (spacing * i)), yPadding, -1), Quaternion.identity);
+            aHeart.transform.SetParent(allCanvas.transform, false);
+        }
     }
 }
