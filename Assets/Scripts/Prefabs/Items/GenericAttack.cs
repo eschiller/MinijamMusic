5cfcbe1251bc66faa309a8a239af9708750f6710
eschiller <eric.schiller@raytheon.com>
2019-01-07 13:17:46 -0500

    adding generic attack script and prefab

---
+++ Assets/Scripts/Prefabs/Items/GenericAttack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericAttack : MonoBehaviour {
+
+    public int attackDamage = 1;
+    public string target = "Enemy";
+    public float lastingTime = .2f;
+
+    private float timeToDeath = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeToDeath += Time.deltaTime;
+        if (timeToDeath > lastingTime) {
+            Destroy(transform.gameObject);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("In attack collision stay.");
+        Debug.Log("target tag is " + other.transform.tag);
+        if (other.transform.tag == "Enemy")
+        {
+            Debug.Log("collide with target");
+            other.gameObject.GetComponent<CharacterManager>().LoseHealth(attackDamage);
+        } else {
+            Debug.Log("But didn't collide with target");
+        }
+    }
+}
