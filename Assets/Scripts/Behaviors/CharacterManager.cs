d173ffed1fc19fd35d5bf5fb7d2b593c71a885ce
eschiller <eric.schiller@raytheon.com>
2019-01-12 19:46:29 -0500

    adding blink on damage to PlayerManager.cs

--- Assets/Scripts/Behaviors/CharacterManager.cs
+++ Assets/Scripts/Behaviors/CharacterManager.cs
@@ -1,24 +1,16 @@
-﻿/*
- * CharacterManager class is intended to house functionality shared across
- * all characters, such asPlayers, Enemies, NPCs, etc... possibly even some 
- * entities such as breakable walls. 
- */
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class CharacterManager : MonoBehaviour {
 
-    public int health = 1;
+    public int health = 3;
     private bool isDead = false;
 
     protected SpriteRenderer myRenderer;
 
-	// Use this for initialization
-	void Start () {
-        myRenderer = GetComponent<SpriteRenderer>();
-
-    }
 
     // Update is called once per frame
     void Update () {
@@ -31,12 +23,14 @@ public class CharacterManager : MonoBehaviour {
 
 
     void FlipRenderer() {
-        myRenderer.enabled = !myRenderer.enabled;
+        GetComponent<SpriteRenderer>().enabled = !myRenderer.enabled;
     }
 
 
     public virtual void LoseHealth (int loss) {
+        Debug.Log("health is " + health);
         health -= loss;
+        StartCoroutine("flashRed");
     }
 
 
@@ -44,14 +38,23 @@ public class CharacterManager : MonoBehaviour {
         health += gain;
     }
 
-    /*
-     * Destroys the game object and blinks it for a specified amount of time
-     */
-    public void Die(float deathDelay = 0.8f, float blinkDelay=.08f) {
+    public void Die() {
+
         //set animation for death
         isDead = true;
-        Destroy(gameObject, deathDelay);
-        InvokeRepeating("FlipRenderer", 0.0f, blinkDelay);
+        Destroy(gameObject, .80f);
+        InvokeRepeating("FlipRenderer", 0.0f, .08f);
 
+
+    }
+
+
+    public IEnumerator flashRed() {
+        for (int i = 0; i < 4; i++) {
+            GetComponent<SpriteRenderer>().color = Color.red;
+            yield return new WaitForSeconds(0.1f);
+            GetComponent<SpriteRenderer>().color = Color.white;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
